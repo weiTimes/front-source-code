d3357306616daa81bf4001abb7992ad5f71c22a5
yewei <1156385527@qq.com>
2021-03-13 22:54:12 +0800

    feat: add react render function

--- mini-react/main.js
+++ mini-react/main.js
@@ -3,18 +3,43 @@ import './style.css';
 const Yreact = {
   createElement,
   createTextElement,
+  render,
 };
 
 const element = Yreact.createElement(
   'div',
   { id: 'foo' },
-  Yreact.createElement('a', null, 'bar'),
+  Yreact.createElement('a', null, '我是通过render插入到root的子元素'),
   Yreact.createElement('b')
 );
 
 const container = document.querySelector('#root');
 
+Yreact.render(element, container);
+
 console.log(element, 'element');
+
+function render(element, container) {
+  // create dom nodes
+  // 普通节点 | 文本节点
+  const dom =
+    element.type === 'TEXT_ELEMENT'
+      ? document.createTextNode('')
+      : document.createElement(element.type);
+
+  const isProperty = (key) => key !== 'children';
+
+  // 将 props 添加到 dom
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach((prop) => (dom[prop] = element.props[prop]));
+
+  // 递归子节点
+  element.props.children.forEach((child) => render(child, dom));
+
+  // 插入到容器
+  container.appendChild(dom);
+}
 
 function createElement(type, props, ...children) {
   return {
